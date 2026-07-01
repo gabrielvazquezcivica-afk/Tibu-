61f2b67685d156846727edc1c6c57e42fb2a1776
GABO <gabrielvazquezcivica@gmail.com>
2026-07-01 11:55:41 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const config = {
     // Números de dueños
     owner: [
         '523310167470',
-        ''
+        '18252535152'
     ],
 
     // IDs / LID de dueños
     ownerLid: [
         '215590228750567',
-        ''
+        '279302511845418'
     ],
 
     PREFIX: '.'
