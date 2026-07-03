2f60c75c85ed107123b4b472e0fb8ce4824ab710
GABO <gabrielvazquezcivica@gmail.com>
2026-07-03 10:14:49 -0600

    Crear grupo-setwelcome.js

---
+++ plugins/grupo-setwelcome.js
@@ -0,0 +1,53 @@
+import fs from 'fs'
+import path from 'path'
+
+const ruta = path.join(process.cwd(), 'database', 'setwelcome.json')
+
+function leer() {
+    try {
+        return JSON.parse(fs.readFileSync(ruta))
+    } catch {
+        return {}
+    }
+}
+
+function guardar(data) {
+    fs.mkdirSync(path.dirname(ruta), { recursive: true })
+    fs.writeFileSync(ruta, JSON.stringify(data, null, 2))
+}
+
+let handler = {}
+
+handler.run = async (sock, m, args) => {
+    const from = m.key.remoteJid
+    const text = args.join(' ')
+
+    if (!text) {
+        return sock.sendMessage(from, {
+            text:
+`Usa:
+
+.setwelcome texto
+
+Variables:
+> @user
+> @group
+> @members`
+        }, { quoted: m })
+    }
+
+    const db = leer()
+    db[from] = text
+    guardar(db)
+
+    sock.sendMessage(from, {
+        text:
+`🌊 Welcome configurado`
+    }, { quoted: m })
+}
+
+handler.command = ['setwelcome']
+handler.help = ['setwelcome <texto>']
+handler.tags = ['grupo']
+
+export default handler
