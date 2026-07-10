7aef1360e4e9a457295874fe4c910cb220b06d4f
GABO <gabrielvazquezcivica@gmail.com>
2026-07-10 04:12:43 -0600

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -116,44 +116,28 @@ handler.run = async (sock, m, args, { commands }) => {
 └───────────────────────────┘`
 
     for (const tag of ordenTags) {
-        if (!grupos[tag]) continue
+    if (!grupos[tag]) continue
 
-        const icono = emojiTag[tag] || '📌'
+    const icono = emojiTag[tag] || '📌'
 
-        texto += `
-
-texto += `
+    texto += `
 
 ${centrarTitulo(icono, tag)}
 ┌───────────────────────────┐`
 
-        function fancy(texto) {
-    const map = {
-        a:'𝐚',b:'𝐛',c:'𝐜',d:'𝐝',e:'𝐞',f:'𝐟',
-        g:'𝐠',h:'𝐡',i:'𝐢',j:'𝐣',k:'𝐤',l:'𝐥',
-        m:'𝐦',n:'𝐧',o:'𝐨',p:'𝐩',q:'𝐪',r:'𝐫',
-        s:'𝐬',t:'𝐭',u:'𝐮',v:'𝐯',w:'𝐰',x:'𝐱',
-        y:'𝐲',z:'𝐳',
-        A:'𝐀',B:'𝐁',C:'𝐂',D:'𝐃',E:'𝐄',F:'𝐅',
-        G:'𝐆',H:'𝐇',I:'𝐈',J:'𝐉',K:'𝐊',L:'𝐋',
-        M:'𝐌',N:'𝐍',O:'𝐎',P:'𝐏',Q:'𝐐',R:'𝐑',
-        S:'𝐒',T:'𝐓',U:'𝐔',V:'𝐕',W:'𝐖',X:'𝐗',
-        Y:'𝐘',Z:'𝐙'
-    }
+    grupos[tag].forEach(cmd => {
 
-    return [...texto]
-        .map(x => map[x] || x)
-        .join('')
-}
-
-grupos[tag].forEach(cmd => {
-    texto += `
-│ ${icono} ${config.PREFIX}${fancy(cmd)}`
-})
+        const partes = cmd.split(' ')
+        const comando = partes.shift()
+        const params = partes.join(' ')
 
         texto += `
+│ ${icono} ${config.PREFIX}${fancy(comando)}${params ? ' ' + params : ''}`
+    })
+
+    texto += `
 └───────────────────────────┘`
-    }
+}
 
     texto += `
 
