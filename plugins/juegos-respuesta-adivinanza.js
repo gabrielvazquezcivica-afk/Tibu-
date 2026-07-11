0077a34e6359cffc5409eada355b7de944423910
GABO <gabrielvazquezcivica@gmail.com>
2026-07-11 08:50:42 -0600

    Actualizar juegos-respuesta-adivinanza.js

--- plugins/juegos-respuesta-adivinanza.js
+++ plugins/juegos-respuesta-adivinanza.js
@@ -1,55 +1,59 @@
 import config from '../config.js'
-import { partidas } from './juegos-adivinanza.js'
+
+if (!global.adivinanzasActivas) {
+    global.adivinanzasActivas = new Map()
+}
 
 let handler = {}
 
 handler.run = async (sock, m, args) => {
-const from = m.key.remoteJid
+    const from = m.key.remoteJid
 
-if (!partidas.has(from)) {
-return sock.sendMessage(from, {
-text:
+    if (!global.adivinanzasActivas.has(from)) {
+        return sock.sendMessage(from, {
+            text:
 `❌ \`No hay ninguna adivinanza activa.\`
 
 💡 Usa:
 \`.adivinanza\`
 
 > ${config.BOT_NAME}`
-}, { quoted: m })
-}
+        }, { quoted: m })
+    }
 
-const respuesta =
-args.join(' ')
-.toLowerCase()
-.trim()
+    const respuesta =
+        args.join(' ')
+            .toLowerCase()
+            .trim()
 
-if (!respuesta) {
-return sock.sendMessage(from, {
-text:
+    if (!respuesta) {
+        return sock.sendMessage(from, {
+            text:
 `❌ \`Debes escribir una respuesta.\`
 
 Ejemplo:
 \`.respuesta reloj\`
 
 > ${config.BOT_NAME}`
-}, { quoted: m })
-}
+        }, { quoted: m })
+    }
 
-const correcta = partidas.get(from)
+    const correcta =
+        global.adivinanzasActivas.get(from)
 
-if (respuesta === correcta) {
+    if (respuesta === correcta) {
 
-partidas.delete(from)
+        global.adivinanzasActivas.delete(from)
 
-await sock.sendMessage(from, {
-react: {
-text: '🎉',
-key: m.key
-}
-})
+        await sock.sendMessage(from, {
+            react: {
+                text: '🎉',
+                key: m.key
+            }
+        })
 
-return sock.sendMessage(from, {
-text:
+        return sock.sendMessage(from, {
+            text:
 `🎉 \`¡Respuesta correcta!\`
 
 🏆 La respuesta era:
@@ -57,24 +61,24 @@ text:
 \`${correcta}\`
 
 > ${config.BOT_NAME}`
-}, { quoted: m })
-}
+        }, { quoted: m })
+    }
 
-await sock.sendMessage(from, {
-react: {
-text: '❌',
-key: m.key
-}
-})
+    await sock.sendMessage(from, {
+        react: {
+            text: '❌',
+            key: m.key
+        }
+    })
 
-return sock.sendMessage(from, {
-text:
+    return sock.sendMessage(from, {
+        text:
 `❌ \`Respuesta incorrecta.\`
 
 🔄 Sigue intentando.
 
 > ${config.BOT_NAME}`
-}, { quoted: m })
+    }, { quoted: m })
 }
 
 handler.command = ['respuesta']
