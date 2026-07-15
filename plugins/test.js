360c14e62c4237a7baf7c5f109a326396fb6d5b5
GABO <gabrielvazquezcivica@gmail.com>
2026-07-15 11:36:11 -0600

    Actualizar test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,15 +1,69 @@
+import {
+    proto,
+    generateWAMessageFromContent
+} from '@whiskeysockets/baileys'
+
 let handler = {}
 
 handler.run = async (sock, m) => {
-    await sock.sendMessage(
+
+    const msg = generateWAMessageFromContent(
         m.key.remoteJid,
-        { text: 'FUNCIONA' },
-        { quoted: m }
+        {
+            viewOnceMessage: {
+                message: {
+                    messageContextInfo: {},
+                    interactiveMessage:
+                    proto.Message.InteractiveMessage.create({
+
+                        body: {
+                            text: '🧪 Prueba de botones Tibu'
+                        },
+
+                        footer: {
+                            text: 'Tibu Bot'
+                        },
+
+                        header: {
+                            title: 'Botones',
+                            hasMediaAttachment: false
+                        },
+
+                        nativeFlowMessage: {
+                            buttons: [
+                                {
+                                    name: 'quick_reply',
+                                    buttonParamsJson: JSON.stringify({
+                                        display_text: '📋 MENU',
+                                        id: '.menu'
+                                    })
+                                },
+                                {
+                                    name: 'quick_reply',
+                                    buttonParamsJson: JSON.stringify({
+                                        display_text: '🎵 PLAYLIST',
+                                        id: '.playlist mc davo'
+                                    })
+                                }
+                            ]
+                        }
+
+                    })
+                }
+            }
+        },
+        {}
+    )
+
+    await sock.relayMessage(
+        m.key.remoteJid,
+        msg.message,
+        { messageId: msg.key.id }
     )
 }
 
-handler.command = ['ping2']
-handler.help = ['ping2']
+handler.command = ['test']
+handler.help = ['test']
 handler.tags = ['informacion']
 handler.menu = true
 
