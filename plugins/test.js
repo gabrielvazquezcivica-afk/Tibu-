52ad7b31d11281790dd50b8da4f40aced2059560
GABO <gabrielvazquezcivica@gmail.com>
2026-07-15 11:21:04 -0600

    Crear test.js

---
+++ plugins/test.js
@@ -0,0 +1,67 @@
+import {
+    proto,
+    generateWAMessageFromContent
+} from '@whiskeysockets/baileys'
+
+let handler = {}
+
+handler.run = async (sock, m) => {
+
+    const msg = generateWAMessageFromContent(
+        m.key.remoteJid,
+        {
+            viewOnceMessage: {
+                message: {
+                    messageContextInfo: {},
+                    interactiveMessage:
+                    proto.Message.InteractiveMessage.create({
+
+                        body: {
+                            text: '🧪 Prueba de botones Tibu'
+                        },
+
+                        footer: {
+                            text: 'Tibu Bot'
+                        },
+
+                        header: {
+                            title: 'Botones',
+                            hasMediaAttachment: false
+                        },
+
+                        nativeFlowMessage: {
+                            buttons: [
+                                {
+                                    name: 'quick_reply',
+                                    buttonParamsJson: JSON.stringify({
+                                        display_text: '📋 MENU',
+                                        id: '.menu'
+                                    })
+                                },
+                                {
+                                    name: 'quick_reply',
+                                    buttonParamsJson: JSON.stringify({
+                                        display_text: '🎵 PLAYLIST',
+                                        id: '.playlist mc davo'
+                                    })
+                                }
+                            ]
+                        }
+
+                    })
+                }
+            }
+        },
+        {}
+    )
+
+    await sock.relayMessage(
+        m.key.remoteJid,
+        msg.message,
+        { messageId: msg.key.id }
+    )
+}
+
+handler.command = ['testbtn']
+
+export default handler
